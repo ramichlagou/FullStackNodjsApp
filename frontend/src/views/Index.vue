af7f16a9590ae136d5a23041770551e3fb71646f
rami chlagou <chlagourami1@gmail.com>
2022-10-09 12:53:27 +0100

    First commi

---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>My First Application Version 0.1</h1>
+    <ProductList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ProductList from "@/components/ProductList.vue";
+
+export default {
+  name: "Index",
+  components: {
+    ProductList,
+  },
+};
+</script>
