4341292b46bb12b84dd458fedf884cae659124c6
rami chlagou <chlagourami1@gmail.com>
2022-10-09 15:17:04 +0100

    yamls

--- product.sql
+++
@@ -1,9 +0,0 @@
---
--- Table structure for table `product`
---
-
-CREATE TABLE product(
-product_id INT(11) PRIMARY KEY AUTO_INCREMENT,
-product_name VARCHAR(200),
-product_price DOUBLE
-)ENGINE=INNODB;
